4103d4030728e02d72c0fbb8685e961c951083a4
LucasFA <lucaslanzarote@gmail.com>
2022-09-23 00:12:30 +0200

    Finish

--- solution1.py
+++ solution1.py
@@ -1,30 +1,30 @@
 import math
 from itertools import ifilter
 
-
-def sieve(n, prime_flag_table = []):
-    """Returns a list holding prime numbers"""
+def sieve(n):
+    """Returns a list holding prime numbers, upto but not including n"""
     # I mean, we could just download a list of primes instead. That would be more performant time and memory wise for large n
-    if prime_flag_table == []:
-        prime_flag_table = [True for _ in range(n + 1)]
-        prime_flag_table[0] = prime_flag_table[1] = False
-    else:
-        prime_flag_table.extend([True for _ in range(len(prime_flag_table), n + 1)]) 
-    true_values_table_it = ifilter(lambda i: prime_flag_table[i], range(2, int(math.sqrt(n))))
+    n = int(n)
+    if n <= 1:
+        return []
+    prime_flag_table = [False for _ in range(2)]
+    prime_flag_table.extend([True for _ in range(2, n)]) 
+    true_values_table_it = ifilter(lambda i: prime_flag_table[i], range(2, int(math.ceil(math.sqrt(n)))))
     for i in true_values_table_it:
-        for j in range(i**2, n + 1, i):
+        for j in range(i**2, n, i):
             prime_flag_table[j] = False
     primes = [idx for idx, value in enumerate(prime_flag_table) if value]
     return primes
 
 def prime_string(length):
     s = ""
-    factor = 2
-    primes = []
+    # we want to underestimate the number of primes in a range so we don't need to start the sieve over again
+    # for more, read https://en.wikipedia.org/wiki/Prime_number_theorem#Table_of_%CF%80(x),_x_/_log_x,_and_li(x)
+    factor = 1.5
+    reasonable_min = 20 + length # 235711
+    estimate_needed =  length/math.log(max(2, length))
     while len(s) < length:
-        reasonable_min = 20 + length # 235711131719
-        PNT_dist = length/math.log(max(2, length)) # see "Prime number theorem"
-        primes = sieve(factor * max(reasonable_min, PNT_dist), primes) 
+        primes = sieve(factor * max(reasonable_min, estimate_needed)) 
         s = list_to_string(primes)
         factor *= 2
     return s[:length]
